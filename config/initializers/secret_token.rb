c788b4a68f611b97e8060588237610d6d48426b4
Paulo Carmona <pcarmona@misena.edu.co>
2013-06-27 15:08:48 -0500

    Inicio App Contrats

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Contrats::Application.config.secret_token = 'c52a02e702a7a4a65f958d08792a3a2b286fb91863d9e06a6fdd7da7af624827951e1d530f342834e12feb374a3ef7bac222c28e6c8a8093904237fa69abbe6d'
